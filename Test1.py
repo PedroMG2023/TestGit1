38fe6f4254bf8bf6209103fd62fbe17d169f3f88
Pedro Miranda <devmgcoding03@gmail.com>
2024-05-18 20:28:10 -0600

    Se agrega funcion que suma dos numeros y regresa el resultado

---
+++ Test1.py
@@ -0,0 +1,11 @@
+def sumar_numeros(num1, num2):
+    return num1 + num2
+
+def main():
+    numero1 = 5
+    numero2 = 10
+    resultado = sumar_numeros(numero1, numero2)
+    print("El resultado de la suma es:", resultado)
+
+
+main()
